9ee7a858af621139168e09c13b9dd1532afc1af0
Steve Goldberg <steve.schnee@gmail.com>
2015-05-10 13:16:25 -0400

    set up midi dropdown

--- js/views/item/midiItemView.js
+++ js/views/item/midiItemView.js
@@ -10,8 +10,38 @@ define([
             
             template: Template,
             
+            initialize: function() {
+                this.midi = false;
+                this.inputs = [];
+                this.requestMidi();
+            },
+            
             onShow: function() {
 
+            },
+            
+            requestMidi: function() {
+                var that = this;
+                var inputs; 
+                navigator.requestMIDIAccess().then(function(access) {
+                    console.log(access);
+                    if(access.inputs && access.inputs.size > 0) {
+                        that.midi = true;
+                        inputs = access.inputs.values();
+                        for (input = inputs.next(); input && !input.done; input = inputs.next()) {
+                            that.inputs.push(input.value);
+                        }
+                        console.log(that.inputs);
+                        that.render();
+                    }
+                });
+            },
+            
+            serializeData: function() {
+                return {
+                    midi: this.midi,
+                    inputs: this.inputs
+                };
             }
             
         });
