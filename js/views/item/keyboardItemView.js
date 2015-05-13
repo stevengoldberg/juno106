b0baa7d038061a6ff50bb5e97ba21c884d4c0179
Steve Goldberg <steve.schnee@gmail.com>
2015-05-12 21:10:44 -0400

    finer fader scale

--- js/views/item/keyboardItemView.js
+++ js/views/item/keyboardItemView.js
@@ -10,13 +10,18 @@ define([
             className: 'keyboard-container',
             
             template: Template,
+            
+            initialize: function() {
+                this.keyArray = ['A', 'W', 'S', 'E', 'D', 'F', 'T', 'G', 'Y', 'H',
+                    'U', 'J', 'K', 'O', 'L', 'P', ';', '\'', '\]', '\\'];
+            },
 
             onShow: function() {
             
                 var keyboard = new QwertyHancock({
                     id: 'js-keyboard',
                     width: 1024,
-                    height: 150,
+                    height: 120,
                     octaves: 2,
                     startNote: 'C3',
                     whiteNotesColour: 'white',
@@ -27,6 +32,8 @@ define([
                 keyboard.keyDown = this.keyDownHandler.bind(this);
                 
                 keyboard.keyUp = this.keyUpHandler.bind(this);
+                
+                //this.labelKeyboard();
             },
             
             keyUpHandler: function(note, frequency) {
@@ -35,6 +42,28 @@ define([
             
             keyDownHandler: function(note, frequency) {
                 this.trigger('noteOn', note, frequency);
+            },
+            
+            labelKeyboard: function() {
+                var that = this;
+                var keys = this.$('li');
+                var label;
+                var color;
+                
+                keys.each(function(i, key) {
+                    color = $(this).data().noteType;
+                    if(color === 'white') {
+                        $(this).css({
+                            position: 'relative'
+                        });
+                    } else {
+                        $(this).css({
+                            'z-index': 2
+                        });
+                    }
+                    label = $('<span class="key-label--' + color + '">' + that.keyArray[i] + '</span>');
+                    $(this).append(label);
+                });
             }
 
         });
