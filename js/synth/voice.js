7830f9ad6d5757e1683883c46d9e26fdcd2db9ae
Steve Goldberg <steve.goldberg@nuix.com>
2015-05-18 11:45:16 -0400

    refactor to fix chorus

--- js/synth/voice.js
+++ js/synth/voice.js
@@ -6,59 +6,98 @@ define([
     'synth/hpf',
     'synth/vca'
 ],
-    
+
     function(App, DCO, VCF, ENV, HPF, VCA) {
         return Backbone.Marionette.Object.extend({
             initialize: function(data) {
-        
+
                 this.createComponents(data);
-                
-                // Make the tuna.js chorus effect responsive to the UI
-                if(!_.has(this.cho, 'chorusToggle')) {
-                    Object.defineProperties(this.cho, {
-                        'chorusToggle': {
-                            'set': function(value) { 
-                                setDetune.call(this, value);
-                                this.cho.chorusLevel = value;
-                                chorusToggle.call(this.cho);
-                            }.bind(this)
-                        }
-                    });
-                }
-                
+
                 // Mix Backbone.Events into the ENV to sync the amp and filter envelopes
                 _.extend(this.env, Backbone.Events);
 
                 // Mix Backbone.Events into the DCO to sync changes to the LFO
                 // and manage oscillator lifetime
                 _.extend(this.dco, Backbone.Events);
-                
+
                 this.createListeners();
-                   
+
                 // Connect nodes
-                connect(this.lfo.pitchMod, this.dco.input);
-                connect(this.lfo.pwmMod, this.dco.pwm);
-                connect(this.lfo.freqMod, this.vcf.detune);
-                connect(this.dco.output, this.hpf.input);
-                connect(this.hpf.output, this.vcf.input);
-                connect(this.vcf.output, this.vca.input);
-                connect(this.vca.output, this.env.input);
-                connect(this.env.output, this.cho.input);
-                
-                function connect(output, input) {
-                    if(_.isArray(output)) {
-                        _.forEach(output, function(outputNode) {
-                            outputNode.connect(input);
-                        });
-                    } else if(_.isArray(input)) {
-                        _.forEach(input, function(inputNode) {
-                            output.connect(inputNode);
-                        });
-                    } else {
-                        output.connect(input);
-                    }
+                this.connect(this.lfo.pitchMod, this.dco.input);
+                this.connect(this.lfo.pwmMod, this.dco.pwm);
+                this.connect(this.lfo.freqMod, this.vcf.detune);
+                this.connect(this.dco.output, this.hpf.input);
+                this.connect(this.hpf.output, this.vcf.input);
+                this.connect(this.vcf.output, this.vca.input);
+                this.connect(this.vca.output, this.env.input);
+                this.connect(this.env.output, this.cho.input);
+
+            },
+
+            connect: function(output, input) {
+                if(_.isArray(output)) {
+                    _.forEach(output, function(outputNode) {
+                        outputNode.connect(input);
+                    });
+                } else if(_.isArray(input)) {
+                    _.forEach(input, function(inputNode) {
+                        output.connect(inputNode);
+                    });
+                } else {
+                    output.connect(input);
                 }
-                
+            },
+
+            createComponents: function(data) {
+                var options = data.synthOptions;
+                // Envelope constants
+                var envConstants = {
+                    envelopeOffset: 0.0015,
+                    attackMax: 3,
+                    decayReleaseMax: 12
+                };
+
+                this.vcf = new VCF({
+                    frequency: options.vcfFreq,
+                    res: options.res,
+                    envelope: options.envelope,
+                    vcfEnv: options.vcfEnv,
+                    envConstants: envConstants,
+                    inverted: options.vcfInverted,
+                    keyFreq: options.frequency,
+                    keyFollow: options.keyFollow
+                });
+
+                this.env = new ENV({
+                    envelope: options.envelope,
+                    maxLevel: options.volume,
+                    envConstants: envConstants
+                });
+
+                this.dco = new DCO({
+                    frequency: options.frequency,
+                    waveform: options.waveform,
+                    lfoPwmEnabled: options.lfoPwmEnabled
+                });
+
+                this.hpf = new HPF({
+                    frequency: options.hpf
+                });
+
+                this.vca = new VCA({
+                    maxLevel: options.volume
+                });
+
+                this.lfo = data.lfo;
+                this.lfo.rate = options.lfoRate;
+                this.lfo.delay = options.lfoDelay;
+                this.lfo.pwmEnabled = options.lfoPwmEnabled;
+                this.lfo.freqMod = options.lfoFreqMod;
+                this.lfo.pitchMod = options.lfoPitchMod;
+                this.lfo.pwmMod = options.waveform.pulseWidth;
+
+                this.cho = data.cho;
+
                 function chorusToggle() {
                     switch(this.chorusLevel) {
                         case 0:
@@ -78,7 +117,7 @@ define([
                             break;
                     }
                 }
-                
+
                 // Additional detune for chorus effect
                 function setDetune(value) {
                     var detune = App.context.createOscillator();
@@ -87,62 +126,25 @@ define([
                     detune.frequency.value = (0.05 * value);
                     gain.gain.value = (5 * value);
                     detune.connect(gain);
-                    connect(gain, this.dco.input);
+                    this.connect(gain, this.dco.input);
                 }
-            },
-            
-            createComponents: function(data) {
-                var options = data.synthOptions;
-                // Envelope constants
-                var envConstants = {
-                    envelopeOffset: 0.0015,
-                    attackMax: 3,
-                    decayReleaseMax: 12
-                };
-                
-                this.vcf = new VCF({
-                    frequency: options.vcfFreq,
-                    res: options.res,
-                    envelope: options.envelope,
-                    vcfEnv: options.vcfEnv,
-                    envConstants: envConstants,
-                    inverted: options.vcfInverted,
-                    keyFreq: options.frequency,
-                    keyFollow: options.keyFollow
-                });
-                
-                this.env = new ENV({
-                    envelope: options.envelope,
-                    maxLevel: options.volume,
-                    envConstants: envConstants
-                });
-                
-                this.dco = new DCO({
-                    frequency: options.frequency,
-                    waveform: options.waveform,
-                    lfoPwmEnabled: options.lfoPwmEnabled
-                });
-                
-                this.hpf = new HPF({
-                    frequency: options.hpf
-                });
-                
-                this.vca = new VCA({
-                    maxLevel: options.volume
-                });
-                
-                this.lfo = data.lfo;
-                this.lfo.rate = options.lfoRate;
-                this.lfo.delay = options.lfoDelay;
-                this.lfo.pwmEnabled = options.lfoPwmEnabled;
-                this.lfo.freqMod = options.lfoFreqMod;
-                this.lfo.pitchMod = options.lfoPitchMod;
-                this.lfo.pwmMod = options.waveform.pulseWidth;
-                
-                this.cho = data.cho;
+
+                // Make the tuna.js chorus effect responsive to the UI
+                if(!_.has(this.cho, 'chorusToggle')) {
+                    Object.defineProperties(this.cho, {
+                        'chorusToggle': {
+                            'set': function(value) {
+                                setDetune.call(this, value);
+                                this.cho.chorusLevel = value;
+                                chorusToggle.call(this.cho);
+                            }.bind(this)
+                        }
+                    });
+                }
+
                 this.cho.chorusToggle = options.chorusLevel;
             },
-            
+
             createListeners: function() {
                 // After all the oscillators are stopped, remove the voice from the pool
                 // and disconnect it from the graph for garbage collection
@@ -150,50 +152,50 @@ define([
                     this.trigger('killVoice');
                     this.disconnect();
                 }.bind(this));
-                
+
                 this.listenTo(this.env, 'attack', function(newAttack) {
                     this.vcf.attack = newAttack;
                 });
-            
+
                 this.listenTo(this.env, 'decay', function(newDecay) {
                     this.vcf.decay = newDecay;
                 });
-            
+
                 this.listenTo(this.env, 'sustain', function(newSustain) {
                     this.vcf.sustain = newSustain;
                 });
-            
+
                 this.listenTo(this.env, 'release', function(newRelease) {
                     this.vcf.release = newRelease;
                 });
-                
+
                 this.listenTo(this.dco, 'destroyed', triggerKillVoice);
-                
+
                 this.listenTo(this.dco, 'pwm', function(pwmValue) {
                     this.lfo.pwmMod = pwmValue;
                 });
-                
+
                 this.listenTo(this.dco, 'lfoPwmEnabled', function(enabled) {
                     this.lfo.pwmEnabled = enabled;
                 });
             },
-            
+
             noteOn: function() {
                 this.lfo.noteOn();
                 this.env.noteOn();
                 this.vcf.noteOn();
             },
-        
+
             noteOff: function() {
                 this.listenTo(this.env, 'noteOff', this.dco.noteOff.bind(this.dco));
                 this.env.noteOff();
                 this.vcf.noteOff();
             },
-            
+
             stealNote: function() {
                 this.dco.noteOff();
             },
-            
+
             disconnect: function() {
                 this.env.output.disconnect(this.cho.input);
             }
